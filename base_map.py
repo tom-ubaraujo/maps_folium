e52f69c2d5fe939717d534a1032f7965c4950dcd
Ugleiston Araújo <ugleiston@gmail.com>
2019-01-13 14:22:05 -0200

    projeto mapas simples, usando folium.

--- base_map.py
+++ base_map.py
@@ -1,18 +1,31 @@
 import folium
+from folium.plugins import MarkerCluster
 import pandas as pd 
 
 #Load data of volcanos/maps
-data = pd.read_csv("Data/Volcanoes_USA.txt")
+data = pd.read_csv("Volcanoes_USA.csv")
 lat = data['LAT']
 lon = data['LON']
 elevation = data['ELEV']
 
 #creating base map
-map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 8)
+map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 5, tiles = "CartoDB dark_matter")
+
+#creating cluster
+marker_cluster = MarkerCluster().add_to(map)
+
+# defining the ,arker color by the elevation
+def vulc_color(elevation):
+    if elevation < 1000:
+        return "green"
+    elif elevation >= 1000 and elevation <= 3000:
+        return "orange"
+    else:
+        return "red"
 
 for lat, lon, elevation in zip(lat, lon, elevation):
     #add markers
-    folium.Marker(location = [lat,lon], popup = str(elevation) + 'm', icon = folium.Icon(color = 'red')).add_to(map)
+    folium.CircleMarker(location = [lat,lon], radius = 9, popup = str(elevation)+'m', fill_color = vulc_color(elevation), color = "gray", fill_opacity = 0.9).add_to(marker_cluster)
 
 #saving the map
 map.save("base_map.html")
